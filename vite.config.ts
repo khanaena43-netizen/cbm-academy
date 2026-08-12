46284610a103369906c434231a60c9b8e7250244
khanaena43-netizen <khanaena43@gmail.com>
2026-08-12 16:27:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-fimport tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-base: '/', plugins: [react(), tailwindcss()],
+base: '/cbm-academy/', plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
